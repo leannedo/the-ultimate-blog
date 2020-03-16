182cd27d1ff601dba04a34af527085000a449cfc
leannedo <ngocdo0912@gmail.com>
2020-03-16 14:37:59 +0200

    add post separate-small-chunks

--- src/data/codenlearns.js
+++ src/data/codenlearns.js
@@ -4,5 +4,11 @@ export default [
         date: '27-02-2020',
         description: '#Java #array #input #assign #compare #replace',
         mdFileName: 'largest-number',
+    },
+    {
+        title: '#2.Separate UI from logic and the main program',
+        date: '16-03-2020',
+        description: '#Java #UI #main #logic',
+        mdFileName: 'separate-small-chunks',
     }
 ]
